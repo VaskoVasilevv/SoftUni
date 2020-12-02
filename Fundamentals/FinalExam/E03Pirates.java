fc7ee5d8c1fa9dc9bd338c10d05b19e97990ab5c
Vasil <71281982+VaskoVasilevv@users.noreply.github.com>
2020-12-02 14:26:39 +0200

    Update E03Pirates.java
    changes from github

--- Fundamentals/FinalExam/E03Pirates.java
+++ Fundamentals/FinalExam/E03Pirates.java
@@ -1,6 +1,5 @@
 package bg.softuni.fundamentals.FinalExam;
 
-import java.awt.geom.GeneralPath;
 import java.util.*;
 
 public class E03Pirates {
@@ -8,29 +7,41 @@ public class E03Pirates {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
-        Map<String, List<Integer>> map = new HashMap<>();
-
-        while (!input.equals("Sail")) {
-            String[] tokens = input.split("\\|\\|");
-            String cities = tokens[0];
-            int population = Integer.parseInt(tokens[1]);
-            int gold = Integer.parseInt(tokens[2]);
-
-            if (!map.containsKey(cities)) {
-                map.put(cities, new ArrayList<>(Arrays.asList(population,gold)));
-
-            }else {
-
-            }
-
-
+        Map<String, int[]> towns = new TreeMap<>();
+        while (!"Sail".equals(input)) {
+            String[] town = input.split("\\|\\|");
+            towns.putIfAbsent(town[0], new int[2]);
+            towns.get(town[0])[0] += Integer.parseInt(town[1].trim());
+            towns.get(town[0])[1] += Integer.parseInt(town[2].trim());
             input = scanner.nextLine();
         }
-        input = scanner.nextLine();
         while (!"End".equals(input)) {
-
-
+            String[] command = input.split("=>");
+            switch (command[0]) {
+                case "Plunder":
+                    System.out.printf("%s plundered! %d gold stolen, %d citizens killed.%n", command[1], Integer.parseInt(command[3].trim()), Integer.parseInt(command[2].trim()));
+                    towns.get(command[1])[0] -= Integer.parseInt(command[2].trim());
+                    towns.get(command[1])[1] -= Integer.parseInt(command[3].trim());
+                    if (towns.get(command[1])[0] == 0 || towns.get(command[1])[1] == 0) {
+                        towns.remove(command[1]);
+                        System.out.printf("%s has been wiped off the map!%n", command[1]);
+                    } break;
+                case "Prosper":
+                    if (Integer.parseInt(command[2].trim()) < 0) {
+                        System.out.println("Gold added cannot be a negative number!");
+                    } else {
+                        towns.get(command[1])[1] += Integer.parseInt(command[2].trim());
+                        System.out.printf("%d gold added to the city treasury. %s now has %d gold.%n", Integer.parseInt(command[2].trim()), command[1], towns.get(command[1])[1]);
+                    } break;
+            }
             input = scanner.nextLine();
+        }
+        if (towns.isEmpty()) {
+            System.out.println("Ahoy, Captain! All targets have been plundered and destroyed!");
+        } else {
+            System.out.printf("Ahoy, Captain! There are %d wealthy settlements to go to:%n", towns.size());
+            towns.entrySet().stream().sorted((one, two) -> Integer.compare(two.getValue()[1], one.getValue()[1]))
+                    .forEach(e -> System.out.printf("%s -> Population: %d citizens, Gold: %d kg%n", e.getKey(), e.getValue()[0], e.getValue()[1]));
         }
     }
 }
