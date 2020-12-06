1c155c3eb953b3e5f404f45742864aa24a859519
VaskoVasilevv <n1ggazz@abv.bg>
2020-12-06 15:36:53 +0200

    commit 06.12.2020

--- Fundamentals/FinalExam/E01AugustWorldTour.java
+++ Fundamentals/FinalExam/E01AugustWorldTour.java
@@ -16,14 +16,14 @@ public class E01AugustWorldTour {
                 case "Add Stop":
                     int insertIndex = Integer.parseInt(commandParts[1]);
                     String insertString = commandParts[2];
-                    if (isValidIndex(allStops,insertIndex)) {
+                    if (isValidIndex(allStops, insertIndex)) {
                         allStops.insert(insertIndex, insertString);
                     }
                     break;
                 case "Remove Stop":
                     int fromIndex = Integer.parseInt(commandParts[1]);
                     int toIndex = Integer.parseInt(commandParts[2]);
-                    if (isValidIndex(allStops,fromIndex) && isValidIndex(allStops,toIndex)) {
+                    if (isValidIndex(allStops, fromIndex) && isValidIndex(allStops, toIndex)) {
                         allStops.replace(fromIndex, toIndex + 1, "");
                     }
                     break;
@@ -31,7 +31,7 @@ public class E01AugustWorldTour {
                     String oldString = commandParts[1];
                     String newString = commandParts[2];
 
-                    allStops = new StringBuilder(allStops.toString().replace(oldString,newString));
+                    allStops = new StringBuilder(allStops.toString().replace(oldString, newString));
                     break;
                 default:
                     System.out.println("Unknown command: " + command);
@@ -42,11 +42,11 @@ public class E01AugustWorldTour {
             command = scanner.nextLine();
 
         }
-        System.out.println("Ready for world tour! Planned stops: "+ allStops);
+        System.out.println("Ready for world tour! Planned stops: " + allStops);
 
     }
 
-    static boolean isValidIndex(StringBuilder s , int index){
-        return index >= 0 && index<s.length();
+    static boolean isValidIndex(StringBuilder s, int index) {
+        return index >= 0 && index < s.length();
     }
 }
