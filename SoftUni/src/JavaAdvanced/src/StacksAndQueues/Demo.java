5405d1899b7e553e21a5a40c34791fc0413a29f8
VaskoVasilevv <n1ggazz@abv.bg>
2021-01-18 13:19:02 +0200

    added on 18.01.21

--- SoftUni/src/JavaAdvanced/src/StacksAndQueues/Demo.java
+++ SoftUni/src/JavaAdvanced/src/StacksAndQueues/Demo.java
@@ -1,21 +1,56 @@
 package StacksAndQueues;
 
 import java.util.ArrayDeque;
-
 import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        String input = scanner.nextLine();
 
-        deque.push(24);
-        Integer element = deque.peek();
-
-        System.out.println(element);
-        System.out.println(deque.isEmpty());
+        ArrayDeque<Character> stack = new ArrayDeque<>();
 
 
+        boolean flag = true;
+        for (int i = 0; i < input.length(); i++) {
+            char symbol = input.charAt(i);
+
+            if (symbol == '{' || symbol == '[' || symbol == '(') {
+                stack.push(symbol);
+            } else if (symbol == '}') {
+                char first = stack.pop();
+
+                if (first == '{') {
+                    continue;
+                } else {
+                    flag = false;
+                    break;
+                }
+            } else if (symbol == ']') {
+                char first = stack.pop();
+
+                if (first == '[') {
+                    continue;
+                } else {
+                    flag = false;
+                    break;
+                }
+            } else if (symbol == ')') {
+                char first = stack.pop();
+
+                if (first == '(') {
+                    continue;
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if (flag) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
