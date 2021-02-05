abb0a8a5aa57e8363c7d075fe1ec77cc55b7f703
VaskoVasilevv <n1ggazz@abv.bg>
2021-02-05 17:51:24 +0200

    added on 04.02.21

--- SoftUni/src/JavaAdvanced/src/MultidimensionalArrays/Exercise/Demo.java
+++ SoftUni/src/JavaAdvanced/src/MultidimensionalArrays/Exercise/Demo.java
@@ -1,30 +1,52 @@
 package MultidimensionalArrays.Exercise;
 
-import com.sun.jdi.event.StepEvent;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] matrixRowAndCol =Arrays.stream ((scanner.nextLine().split(" "))).mapToInt(Integer::parseInt).toArray();
+        String[] tokens = scanner.nextLine().split("\\s+");
 
-        int rows = matrixRowAndCol[0];
-        int cols = matrixRowAndCol[1];
+        String[][] matrix = new String[Integer.parseInt(tokens[0])][Integer.parseInt(tokens[1])];
 
-       String [][] matrix = new String[rows][cols];
-        char start = 'a';
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                String palindrome = "" +start + (char) (start+col) + start;
-                matrix[row][col] = palindrome;
+
+    }
+
+    private static void printMatrix(String[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
             }
-            start = (char) (start + 1);
-
+            System.out.println();
         }
-        System.out.println();
+    }
 
+    private static int[][] fillMatrix(int rowsAndCols, String pattern) {
+
+        int num = 1;
+        int[][] matrix = new int[rowsAndCols][rowsAndCols];
+        if (pattern.equals("A")) {
+
+            for (int col = 0; col < matrix.length; col++) {
+                for (int row = 0; row < matrix[col].length; row++) {
+                    matrix[row][col] = num++;
+
+                }
+            }
+        } else if (pattern.equals("B")) {
+            for (int col = 0; col < matrix.length; col++) {
+                if (col % 2 == 0) {
+                    for (int row = 0; row < matrix[col].length; row++) {
+                        matrix[row][col] = num++;
+                    }
+                } else {
+                    for (int row = matrix[col].length - 1; row >= 0; row--) {
+                        matrix[row][col] = num++;
+                    }
+                }
+            }
+        }
+        return matrix;
     }
 }
