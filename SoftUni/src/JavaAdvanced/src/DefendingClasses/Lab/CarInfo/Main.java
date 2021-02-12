a71b9d9732eb547d0839e084f9b62fa11d2dfe9d
VaskoVasilevv <n1ggazz@abv.bg>
2021-02-12 20:54:20 +0200

    add 01.02.21

--- SoftUni/src/JavaAdvanced/src/DefendingClasses/Lab/CarInfo/Main.java
+++ SoftUni/src/JavaAdvanced/src/DefendingClasses/Lab/CarInfo/Main.java
@@ -1,12 +1,12 @@
-package DefendingClasses.Lab;
+package DefendingClasses.Lab.CarInfo;
 
-import DefendingClasses.Lab.Class.Car;
+import DefendingClasses.Lab.CarInfo.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class CarInfo {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
