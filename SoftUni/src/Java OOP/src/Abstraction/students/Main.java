4018cab080dd8581aa8564e40db38375dfd2466b
VaskoVasilevv <n1ggazz@abv.bg>
2021-02-24 16:41:19 +0200

    new homework

--- SoftUni/src/Java OOP/src/Abstraction/students/Main.java
+++ SoftUni/src/Java OOP/src/Abstraction/students/Main.java
@@ -6,13 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         StudentSystem studentSystem = new StudentSystem();
-        while (true)
-        {
-            String[] input = scanner.nextLine().split(" ");
-            if (input[0].equals("Exit")){
-                break;
-            }
-            studentSystem.parseCommand(input);
+
+        String input = scanner.nextLine();
+        while (!input.equals("Exit")) {
+            String[] tokens = input.split("\\s+");
+            System.out.println(studentSystem.parseCommand(tokens));
+            input = scanner.nextLine();
         }
     }
 }
