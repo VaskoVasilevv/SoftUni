1c27fa0fa6b1fb168978e66d3018f1e9402ac52c
VaskoVasilevv <n1ggazz@abv.bg>
2021-03-03 10:45:24 +0200

    new homework

---
+++ SoftUni/src/Java OOP/src/Inheritance/Lab/RandomArrayList/Main.java
@@ -0,0 +1,4 @@
+package Inheritance.Lab.RandomArrayList;
+
+public class Main {
+}
---
+++ SoftUni/src/Java OOP/src/Inheritance/Lab/SingleInheritence/Animal.java
@@ -0,0 +1,7 @@
+package Inheritance.Lab.SingleInheritence;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
