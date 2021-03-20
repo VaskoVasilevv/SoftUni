8c431de989f279b161fd7a198f24300e35bed02c
VaskoVasilevv <n1ggazz@abv.bg>
2021-03-20 10:16:21 +0200

    new homework

---
+++ SoftUni/src/Java OOP/src/barracksWars/core/commands/Command.java
@@ -0,0 +1,18 @@
+package barracksWars.core.commands;
+
+import barracksWars.interfaces.Executable;
+
+public abstract class Command implements Executable {
+    private String[] data;
+
+
+    protected Command(String[] data) {
+        this.data = data;
+
+    }
+
+    public String[] getData() {
+        return this.data;
+    }
+
+}
