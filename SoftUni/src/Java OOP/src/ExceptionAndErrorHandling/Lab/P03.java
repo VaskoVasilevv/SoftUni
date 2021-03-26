5a1bf505fbd109abfb9d5226abf5574808162326
VaskoVasilevv <n1ggazz@abv.bg>
2021-03-26 07:39:19 +0200

    new homework

---
+++ SoftUni/src/Java OOP/src/ExceptionAndErrorHandling/Lab/P03.java
@@ -0,0 +1,11 @@
+package ExceptionAndErrorHandling.Lab;
+
+import java.util.Scanner;
+
+public class P03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
