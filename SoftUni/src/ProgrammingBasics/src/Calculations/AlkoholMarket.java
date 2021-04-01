b914655fef7cd09f0896b029d752b84970c2ecb4
VaskoVasilevv <n1ggazz@abv.bg>
2021-04-01 19:33:16 +0300

    new homework

--- SoftUni/src/ProgrammingBasics/src/Calculations/AlkoholMarket.java
+++ SoftUni/src/ProgrammingBasics/src/Calculations/AlkoholMarket.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 
 public class AlkoholMarket {
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
-            Double whiskeyPrice = Double.parseDouble(scanner.nextLine());
-            Double colBeer = Double.parseDouble(scanner.nextLine());
-            Double colWine = Double.parseDouble(scanner.nextLine());
-            Double colRakia = Double.parseDouble(scanner.nextLine());
-            Double colWhiskey = Double.parseDouble(scanner.nextLine());
 
-            double rakiaPrice = whiskeyPrice / 2;
-            double winePrice = rakiaPrice * 0.60;
-            double beerPrice = rakiaPrice * 0.20;
+        Scanner scanner = new Scanner(System.in);
 
-            double totalPrice = (whiskeyPrice * colWhiskey) + (colBeer * beerPrice) + (colWine * winePrice)
-                    + (colRakia * rakiaPrice);
+        double whiskeyPrice = Double.parseDouble(scanner.nextLine());
+        double colBeer = Double.parseDouble(scanner.nextLine());
+        double colWine = Double.parseDouble(scanner.nextLine());
+        double colRakia = Double.parseDouble(scanner.nextLine());
+        double colWhiskey = Double.parseDouble(scanner.nextLine());
 
-            System.out.printf("%.2f",totalPrice);
-        }
+        double rakiaPrice = whiskeyPrice / 2;
+        double winePrice = rakiaPrice * 0.60;
+        double beerPrice = rakiaPrice * 0.20;
 
+        double totalPrice = (whiskeyPrice * colWhiskey) + (colBeer * beerPrice) + (colWine * winePrice)
+                + (colRakia * rakiaPrice);
+
+        System.out.printf("%.2f", totalPrice);
+    }
 }
+
+
