d42c83e9d61ca4da8c6701e908398c45c51d09b0
VaskoVasilevv <n1ggazz@abv.bg>
2021-04-11 15:59:05 +0300

    new homework

---
+++ SoftUni/src/Java OOP/src/ExamPreparation_1/cresla/models/CryoReactor.java
@@ -0,0 +1,16 @@
+package ExamPreparation_1.cresla.models;
+
+public class CryoReactor extends BaseReactor {
+
+    private int cryoProductionIndex;
+
+    public CryoReactor(int id, int moduleCapacity, int cryoProductionIndex) {
+        super(id, moduleCapacity);
+        this.cryoProductionIndex = cryoProductionIndex;
+    }
+
+    @Override
+    public long getTotalEnergyOutput() {
+        return super.getTotalEnergyOutput() * this.cryoProductionIndex;
+    }
+}
