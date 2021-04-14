7ac85ab1b68b6977d5d073b0a91a572d5f5e7024
VaskoVasilevv <n1ggazz@abv.bg>
2021-04-14 20:36:45 +0300

    new homework

---
+++ SoftUni/src/Java OOP/src/ExamPreparation_1/cresla/models/HeatProcessor.java
@@ -0,0 +1,7 @@
+package ExamPreparation_1.cresla.models;
+
+public class HeatProcessor extends BaseAbsorberModule {
+    public HeatProcessor(int id, int heatAbsorbing) {
+        super(id, heatAbsorbing);
+    }
+}
