e0bc9fdc87017d71909d81fa3e9e2eb72610f26d
VaskoVasilevv <n1ggazz@abv.bg>
2021-04-30 19:57:17 +0300

    new homework

---
+++ src/JavaOOP/src/EXAMS/spaceStation/repositories/AstronautRepository.java
@@ -0,0 +1,40 @@
+package EXAMS.spaceStation.repositories;
+
+import EXAMS.spaceStation.models.astronauts.Astronaut;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class AstronautRepository implements Repository<Astronaut> {
+    private Collection<Astronaut> astronauts;
+
+    public AstronautRepository() {
+        this.astronauts = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<Astronaut> getModels() {
+        return Collections.unmodifiableCollection(astronauts);
+    }
+
+    @Override
+    public void add(Astronaut model) {
+                astronauts.add(model);
+    }
+
+    @Override
+    public boolean remove(Astronaut model) {
+        return astronauts.remove(model);
+    }
+
+    @Override
+    public Astronaut findByName(String name) {
+        for (Astronaut astronaut : astronauts) {
+            if (astronaut.getName().equals(name)){
+                return astronaut;
+            }
+        }
+        return null;
+    }
+}
