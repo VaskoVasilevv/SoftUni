e9216a6a68e4842a7be788bcce947eb3c3f8ac51
VaskoVasilevv <n1ggazz@abv.bg>
2021-05-11 17:14:49 +0300

    new homework

---
+++ src/JavaOOP/src/DesignPatterns/Ex/observer/ISubject.java
@@ -0,0 +1,8 @@
+package DesignPatterns.Ex.observer;
+
+public interface ISubject {
+    void registerObserver(IObserver observer);
+    void removeObserver(IObserver observer);
+    void notifyObservers();
+    void changeState(String state);
+}
