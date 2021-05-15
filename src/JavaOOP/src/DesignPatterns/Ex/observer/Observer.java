9ffcfe9501da041909caacea2d8f45393ca3904b
VaskoVasilevv <n1ggazz@abv.bg>
2021-05-15 11:55:32 +0300

    new homework

---
+++ src/JavaOOP/src/DesignPatterns/Ex/observer/Observer.java
@@ -0,0 +1,9 @@
+package DesignPatterns.Ex.observer;
+
+public class Observer implements IObserver {
+
+    @Override
+    public void update() {
+        System.out.println("Hurray, I'm updated!");
+    }
+}
