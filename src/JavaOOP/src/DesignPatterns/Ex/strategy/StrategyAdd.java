fb2a5cba0e4ed9f1721814b23111370aa0313408
VaskoVasilevv <n1ggazz@abv.bg>
2021-05-21 22:44:13 +0300

    new homework

---
+++ src/JavaOOP/src/DesignPatterns/Ex/strategy/StrategyAdd.java
@@ -0,0 +1,8 @@
+package DesignPatterns.Ex.strategy;
+
+public class StrategyAdd implements Strategy{
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 + num2;
+    }
+}
