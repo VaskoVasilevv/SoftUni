b5ba687dfa10b4ba5e02ba9a1a2c74c9c2b57fff
VaskoVasilevv <n1ggazz@abv.bg>
2021-06-05 16:09:19 +0300

    new homework

---
+++ src/Fundamentals/src/MidEXAM/Шабломи.java
@@ -0,0 +1,32 @@
+package MidEXAM;
+
+public class Шабломи {
+    public static void main(String[] args) {
+
+        // Принтира само числа разделени по ШПАЦИА Лист!!
+        // System.out.println(numbers.toString().replaceAll("[\\[\\],]",""));
+        //System.out.print(Arrays.toString(new List[]{numbers}).replaceAll("[\\[\\]]", "").replaceAll(", ", "|"));
+
+
+        //        Принтиране на инт масив!!
+//        System.out.println(Arrays.toString(currentState).replaceAll("[\\[\\],]",""));
+
+
+//        Принтиране до края без символ на края
+//        for (int i = 0; i < numbers.size(); i++) {
+//            System.out.print(numbers.get(i));
+//            if (i < numbers.size() - 1) {
+//                System.out.print("|");
+//            }
+//        }
+
+//        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+//                .map(Integer::parseInt)
+//                .collect(Collectors.toList());
+
+//        int [] currentState = Arrays.stream(scanner.nextLine().split("\\s+"))
+//                .mapToInt(Integer::parseInt).toArray();
+
+
+    }
+}
