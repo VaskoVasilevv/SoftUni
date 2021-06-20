e67f207f43e248b134b80aa1ab22913613f355ca
VaskoVasilevv <n1ggazz@abv.bg>
2021-06-20 08:38:58 +0300

    new homework

---
+++ src/Fundamentals/src/Data/Types/TownInfoLab.java
@@ -0,0 +1,16 @@
+package Data.Types;
+
+import java.util.Scanner;
+
+public class TownInfoLab {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String city = scanner.nextLine();
+        int population = Integer.parseInt(scanner.nextLine());
+        int people = Integer.parseInt(scanner.nextLine());
+
+        System.out.printf("Town %s has population of %d and area %d square km.",city,population,people);
+    }
+}
