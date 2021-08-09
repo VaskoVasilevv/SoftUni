a6a2b842c55dbbf145dce0085bd82fc65fa1e9ee
VaskoVasilevv <n1ggazz@abv.bg>
2021-08-09 08:42:17 +0300

    new homework

---
+++ src/SpringData/Exam2/src/main/java/softuni/exam/instagraphlite/repository/PictureRepository.java
@@ -0,0 +1,21 @@
+package softuni.exam.instagraphlite.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.instagraphlite.models.entity.Picture;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PictureRepository extends JpaRepository<Picture,Long> {
+
+//    boolean findByPath(String path);
+    boolean existsByPath(String path);
+
+    Optional<Picture> findByPath(String path);
+
+
+    List<Picture> findAllBySizeGreaterThanOrderBySize(BigDecimal size);
+}
