f00606c45642c86111643955c461f7c91195c11b
VaskoVasilevv <n1ggazz@abv.bg>
2021-08-28 21:57:52 +0300

    new homework

--- src/SpringData/HibernateCodeFirst/src/main/java/entity/Student.java
+++ src/SpringData/HibernateCodeFirst/src/main/java/entity/Student.java
@@ -2,7 +2,9 @@ package entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Entity
 @Table(name = "students")
@@ -10,8 +12,18 @@ public class Student extends User{
 
     private Float avgGrade;
     private Integer attendance;
+    private Set<Course> courses;
 
     public Student() {
+    }
+
+    @ManyToMany(mappedBy = "students")
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
     }
 
     @Column(name = "average_grade")
