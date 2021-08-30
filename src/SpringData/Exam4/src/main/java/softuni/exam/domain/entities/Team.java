9cf5f15b69db23684dd692447df59e783c6d7bce
VaskoVasilevv <n1ggazz@abv.bg>
2021-08-30 09:56:35 +0300

    new homework

---
+++ src/SpringData/Exam4/src/main/java/softuni/exam/domain/entities/Team.java
@@ -0,0 +1,40 @@
+package softuni.exam.domain.entities;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "teams")
+public class Team  extends BaseEntity{
+
+    private String name;
+    private Picture picture;
+    private Set<Player> players;
+
+    @OneToMany(mappedBy = "team",fetch = FetchType.EAGER)
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
+
+    @Column(nullable = false,length = 20,unique = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToOne
+    public Picture getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Picture picture) {
+        this.picture = picture;
+    }
+}
