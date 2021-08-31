91656adb01cb06d8837af50c8c98637335a1f657
VaskoVasilevv <n1ggazz@abv.bg>
2021-08-31 13:31:49 +0300

    new homework

---
+++ src/SpringData/Exam4/src/main/java/softuni/exam/domain/dto/TeamSeedRootDto.java
@@ -0,0 +1,26 @@
+package softuni.exam.domain.dto;
+
+import com.google.gson.annotations.Expose;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "teams")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class TeamSeedRootDto {
+
+    @Expose
+    @XmlElement(name = "team")
+    private List<TeamSeedDto> teams;
+
+    public List<TeamSeedDto> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<TeamSeedDto> teams) {
+        this.teams = teams;
+    }
+}
