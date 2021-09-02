a671a709aa7332360ac0bcb275310ec58af7d86e
VaskoVasilevv <n1ggazz@abv.bg>
2021-09-02 18:25:42 +0300

    new homework

---
+++ src/SpringData/Exam3/src/main/java/softuni/exam/models/dto/TicketSeedDto.java
@@ -0,0 +1,88 @@
+package softuni.exam.models.dto;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import java.math.BigDecimal;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class TicketSeedDto {
+
+    @XmlElement(name = "serial-number")
+    private String serialNumber;
+    private BigDecimal price;
+    @XmlElement(name = "take-off")
+    private String takeOff;
+    @XmlElement(name = "from-town")
+    private TownNameDto fromTown;
+    @XmlElement(name = "to-town")
+    private TownNameDto toTown;
+    @XmlElement(name = "passenger")
+    private PassengerEmailDto passenger;
+    @XmlElement(name = "plane")
+    private PlaneRegisterNumberDto plane;
+
+    public TicketSeedDto() {
+    }
+
+    public PassengerEmailDto getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(PassengerEmailDto passenger) {
+        this.passenger = passenger;
+    }
+
+    public PlaneRegisterNumberDto getPlane() {
+        return plane;
+    }
+
+    public void setPlane(PlaneRegisterNumberDto plane) {
+        this.plane = plane;
+    }
+
+    @Size(min = 2)
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    @Positive
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+
+    public String getTakeOff() {
+        return takeOff;
+    }
+
+    public void setTakeOff(String takeOff) {
+        this.takeOff = takeOff;
+    }
+
+    public TownNameDto getFromTown() {
+        return fromTown;
+    }
+
+    public void setFromTown(TownNameDto fromTown) {
+        this.fromTown = fromTown;
+    }
+
+    public TownNameDto getToTown() {
+        return toTown;
+    }
+
+    public void setToTown(TownNameDto toTown) {
+        this.toTown = toTown;
+    }
+}
