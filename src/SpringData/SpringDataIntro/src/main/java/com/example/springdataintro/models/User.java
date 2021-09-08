d81957c8eea2dbd0271f4f746bff83e81ffe6909
VaskoVasilevv <n1ggazz@abv.bg>
2021-09-08 10:17:25 +0300

    new homework

---
+++ src/SpringData/SpringDataIntro/src/main/java/com/example/springdataintro/models/User.java
@@ -0,0 +1,54 @@
+package com.example.springdataintro.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+
+    private int age;
+
+    @OneToMany(mappedBy = "user")
+    private List<Account> accounts;
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
