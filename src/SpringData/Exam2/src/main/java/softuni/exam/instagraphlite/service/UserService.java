5b777706b3263b203d61abcc3aaf648dfda349cc
VaskoVasilevv <n1ggazz@abv.bg>
2021-09-13 21:57:20 +0300

    new homework

---
+++ src/SpringData/Exam2/src/main/java/softuni/exam/instagraphlite/service/UserService.java
@@ -0,0 +1,16 @@
+package softuni.exam.instagraphlite.service;
+
+import softuni.exam.instagraphlite.models.entity.User;
+
+import java.io.IOException;
+
+public interface UserService {
+    Boolean аreImported();
+    String readFromFileContent() throws IOException;
+    String importUsers() throws IOException;
+    String exportUsersWithTheirPosts();
+
+    User getUserByUsername(String username);
+
+    boolean existByusername(String username);
+}
