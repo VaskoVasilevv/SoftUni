b3669577880860a3b701732370d004bc595c3898
VaskoVasilevv <n1ggazz@abv.bg>
2021-10-05 18:37:44 +0300

    new homework

---
+++ SpringFundamentals/WebApplication/src/main/bg/softuni/pathfinder/model/entity/PictureEntity.java
@@ -0,0 +1,54 @@
+package bg.softuni.pathfinder.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pictures")
+public class PictureEntity extends BaseEntity{
+
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String url;
+    @ManyToOne
+    private UserEntity author;
+    @ManyToOne
+    private RouteEntity route;
+
+    public PictureEntity() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public UserEntity getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(UserEntity author) {
+        this.author = author;
+    }
+
+    public RouteEntity getRoute() {
+        return route;
+    }
+
+    public void setRoute(RouteEntity route) {
+        this.route = route;
+    }
+}
