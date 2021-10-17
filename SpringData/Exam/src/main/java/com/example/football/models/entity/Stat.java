869687cf306b63a641fb38661e263ef942566a6c
VaskoVasilevv <n1ggazz@abv.bg>
2021-10-17 18:16:42 +0300

    new homework

---
+++ SpringData/Exam/src/main/java/com/example/football/models/entity/Stat.java
@@ -0,0 +1,51 @@
+package com.example.football.models.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "stats")
+public class Stat extends BaseEntity {
+
+    private float shooting;
+    private float passing;
+    private float endurance;
+
+    private Set<Player> players;
+
+    @OneToMany(mappedBy = "stat",fetch = FetchType.EAGER)
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
+
+    public float getShooting() {
+        return shooting;
+    }
+
+    public void setShooting(float shooting) {
+        this.shooting = shooting;
+    }
+
+    public float getPassing() {
+        return passing;
+    }
+
+    public void setPassing(float passing) {
+        this.passing = passing;
+    }
+
+    public float getEndurance() {
+        return endurance;
+    }
+
+    public void setEndurance(float endurance) {
+        this.endurance = endurance;
+    }
+}
