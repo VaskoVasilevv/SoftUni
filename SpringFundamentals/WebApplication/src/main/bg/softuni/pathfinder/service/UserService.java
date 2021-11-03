7fc413ed39fdcd95e66e45611309980a649010a1
VaskoVasilevv <n1ggazz@abv.bg>
2021-11-03 11:50:11 +0200

    new homework

---
+++ SpringFundamentals/WebApplication/src/main/bg/softuni/pathfinder/service/UserService.java
@@ -0,0 +1,16 @@
+package bg.softuni.pathfinder.service;
+
+import bg.softuni.pathfinder.model.service.UserServiceModel;
+
+public interface UserService {
+
+
+    void registerUser(UserServiceModel userServiceModel);
+
+    UserServiceModel findUserByUsernameAndPassword(String username, String password);
+
+    void loginUser(Long id, String username);
+
+    void logout();
+
+}
