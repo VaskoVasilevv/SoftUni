a22d03bb6bfbd60d876582bfddaa88431a637331
VaskoVasilevv <n1ggazz@abv.bg>
2021-11-05 14:43:41 +0200

    new homework

---
+++ SpringData/AutoMappingObj/src/main/java/com/example/homework/util/ValidationUtil.java
@@ -0,0 +1,9 @@
+package com.example.homework.util;
+
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
+public interface ValidationUtil {
+   <E> Set<ConstraintViolation<E>> violation(E entity);
+   <E> boolean isValid(E entity);
+}
