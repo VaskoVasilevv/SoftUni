5798a7343fc007b8fb139797f123296100251b46
VaskoVasilevv <n1ggazz@abv.bg>
2021-12-16 17:09:27 +0200

    new homework

---
+++ SpringFundamentals/exam/src/main/java/com/example/exam/model/view/ShipViewModel.java
@@ -0,0 +1,31 @@
+package com.example.exam.model.view;
+
+public class ShipViewModel {
+    private String name;
+    private Long power;
+    private Long health;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPower() {
+        return power;
+    }
+
+    public void setPower(Long power) {
+        this.power = power;
+    }
+
+    public Long getHealth() {
+        return health;
+    }
+
+    public void setHealth(Long health) {
+        this.health = health;
+    }
+}
