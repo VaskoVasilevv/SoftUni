f5ec8323517e0cc7d3012acffe8b8b64a3112f5b
VaskoVasilevv <n1ggazz@abv.bg>
2021-12-19 22:14:27 +0200

    new homework

---
+++ SpringFundamentals/MusicExam/src/main/java/com/example/musicdb/model/entity/UserEntity.java
@@ -0,0 +1,57 @@
+package com.example.musicdb.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity{
+    private String username;
+    private String fullName;
+    private String password;
+    private String email;
+
+    public UserEntity() {
+    }
+
+    @Column(name = "username", unique = true)
+    public String getUsername() {
+        return username;
+    }
+
+    public UserEntity setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    @Column(name = "full_name")
+    public String getFullName() {
+        return fullName;
+    }
+
+    public UserEntity setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public UserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    @Column(name = "email", unique = true)
+    public String getEmail() {
+        return email;
+    }
+
+    public UserEntity setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+}
