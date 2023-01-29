22e06ebeabfade0eea6fe7022f4e679281cc4e3b
Vasil <n1ggazz@abv.bg>
2023-01-30 00:01:35 +0200

    new homework

---
+++ SpringData/Exam3/src/main/java/softuni/exam/models/entity/Passenger.java
@@ -0,0 +1,81 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "passengers")
+public class Passenger extends BaseEntity {
+
+    private Integer age;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private Town town;
+    private Set<Ticket> tickets;
+
+    public Passenger() {
+    }
+    @OneToMany(mappedBy = "passenger",fetch = FetchType.EAGER)
+    public Set<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(Set<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    @ManyToOne
+    public Town getTown() {
+        return town;
+    }
+
+    public void setTown(Town town) {
+        this.town = town;
+    }
+
+    @Column
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Column(unique = true)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    @Column(name = "phone_number")
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
